fab29619bb977983d2b201736a8217ab0fae6b5f
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-02-04 19:36:38 -0800

    Fix: Stop loop when correct answer is given

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -8,14 +8,39 @@ public class Main {
     public static void main(String[] args) {
         //Generate a random number
         Random rand = new Random();
-        int randomInteger = rand.nextInt(100);
+        // Assign random number to variable
+        int randomInteger = rand.nextInt(10);
+        System.out.println(randomInteger);
+        // assign user input to variable
         int userChoice = userInput();
-
+        // determine if the user won
+        boolean answer = wonTheGame(randomInteger, userChoice);
+        // if the user did not win continue the game
+        // ask for
+        while (!answer) {
+            userChoice = userInput();
+            answer =wonTheGame(randomInteger, userChoice);
+        }
+        System.out.println("You Won!!");
     }
+
     public static Integer userInput() {
         System.out.println("guess a number");
         Scanner input = new Scanner(System.in);
         return input.nextInt();
 
-    };
+    }
+
+    ;
+
+    public static Boolean wonTheGame(int randomInteger, int number) {
+        if (number > randomInteger) {
+            return false;
+        } else if (number < randomInteger) {
+            return false;
+        } else return true;
+
+    }
+
+
 }
