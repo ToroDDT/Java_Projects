95df3640bf571f3fed51a8412a91207a8d259ec8
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-02-04 18:10:23 -0800

    Display message and read input

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -1,6 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.util.random;
+import java.sql.SQLOutput;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +9,13 @@ public class Main {
         //Generate a random number
         Random rand = new Random();
         int randomInteger = rand.nextInt(100);
+        int userChoice = userInput();
 
     }
+    public static Integer userInput() {
+        System.out.println("guess a number");
+        Scanner input = new Scanner(System.in);
+        return input.nextInt();
+
+    };
 }
